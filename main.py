93a566a62b7ace847a0d4f1b09e3dd93d8a057e8
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-11-27 14:29:42 +0100

    show transformed sudoku in separate window

--- main.py
+++ main.py
@@ -1,87 +1,129 @@
-import cv2
+from __future__ import division
+from cv2 import *
 import numpy as np
 
 
 def cmp_height(x, y):
-    _, _, _, hx = cv2.boundingRect(x)
-    _, _, _, hy = cv2.boundingRect(y)
+    _, _, _, hx = boundingRect(x)
+    _, _, _, hy = boundingRect(y)
     return hy - hx
 
 
 def cmp_width(x, y):
-    _, _, wx, _ = cv2.boundingRect(x)
-    _, _, wy, _ = cv2.boundingRect(y)
+    _, _, wx, _ = boundingRect(x)
+    _, _, wy, _ = boundingRect(y)
     return wy - wx
 
 
-def solve_sudoku_in_picture(filename):
-    # read
-    original = cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+def process(frame):
+    # convert to grayscale
+    img = cvtColor(frame, COLOR_BGR2GRAY)
 
     # threshold
-    img = cv2.adaptiveThreshold(original, 255,
-                                cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                cv2.THRESH_BINARY, 11, 2)
+    img = adaptiveThreshold(img, 255,
+                            ADAPTIVE_THRESH_GAUSSIAN_C,
+                            THRESH_BINARY, 11, 2)
 
-    img = cv2.medianBlur(img, 3)
-    cv2.imshow('winname', img)
+    # median
+    img = medianBlur(img, 3)
 
     # find contours
-    inv = cv2.bitwise_not(img)
-    contours, hierarchy = cv2.findContours(
-        inv, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    inv = bitwise_not(img)
+    contours, hierarchy = findContours(inv, RETR_TREE,
+                                       CHAIN_APPROX_SIMPLE)
 
-    # select biggest contour
-    biggest_contour = contours[0]
-    for c in contours:
-        if cv2.contourArea(c) > cv2.contourArea(biggest_contour):
-            biggest_contour = c
+    # try to find the sudoku
+    sudoku_area = 0
+    sudoku_contour = None
+    for cnt in contours:
+        area = contourArea(cnt)
+        x, y, w, h = boundingRect(cnt)
+        if (0.7 < w / h < 1.3            # aspect ratio
+                and area > 150 * 150     # minimal area
+                and area > sudoku_area   # biggest area on screen
+                and area > .5 * w * h):  # fills bounding rect
+            sudoku_area = area
+            sudoku_contour = cnt
+
+    if sudoku_contour is not None:
+        perimeter = arcLength(sudoku_contour, True)
+        approx = approxPolyDP(sudoku_contour, 0.1 * perimeter, True)
+        if len(approx) == 4:
+            square = np.float32([[450, 0], [0, 0], [0, 450], [450, 450]])
+            approx = np.float32([i[0] for i in approx])
+
+            m = getPerspectiveTransform(approx, square)
+            out = warpPerspective(frame, m, (450, 450))
+            imshow('winname', out)
+
+        # show contour of sudoku in original video
+        # drawContours(frame, [sudoku_contour], 0, 255)
 
     # create mask
-    mask = np.zeros(img.shape, np.uint8)
-    cv2.drawContours(mask, [biggest_contour], 0, 255, -1)
-    mask_inv = cv2.bitwise_not(mask)
+    # mask = np.zeros(img.shape, np.uint8)
+    # drawContours(mask, [biggest_contour], 0, 255, -1)
+    # mask_inv = bitwise_not(mask)
 
     # show only sudoku on white background
-    img = cv2.bitwise_or(mask_inv, img)
+    # img = bitwise_or(mask_inv, img)
+    # imshow('sudooku', img)
 
     # sobel x-axis
-    sobel_x = cv2.Sobel(img, -1, 1, 0)
-    kernel_x = np.array([[1]] * 15, dtype='uint8')
+    # sobel_x = Sobel(img, -1, 1, 0)
+    # kernel_x = np.array([[1]] * 15, dtype='uint8')
 
     # closing x-axis
-    sobel_x = cv2.dilate(sobel_x, kernel_x)
+    # sobel_x = dilate(sobel_x, kernel_x)
 
     # generate mask for x
-    contours, hierarchy = cv2.findContours(
-        sobel_x, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    sorted_contours = sorted(contours, cmp=cmp_height)
+    # contours, hierarchy = findContours(
+    #     sobel_x, RETR_TREE, CHAIN_APPROX_SIMPLE)
+    # sorted_contours = sorted(contours, cmp=cmp_height)
 
     # fill biggest 10 on mask
-    mask_x = np.zeros(img.shape, np.uint8)
-    for c in sorted_contours[:10]:
-        cv2.drawContours(mask_x, [c], 0, 255, -1)
+    # mask_x = np.zeros(img.shape, np.uint8)
+    # for c in sorted_contours[:10]:
+    #     drawContours(mask_x, [c], 0, 255, -1)
 
     # sobel y-axis
-    sobel_y = cv2.Sobel(img, -1, 0, 1)
-    kernel_y = np.array([[[1]] * 15], dtype='uint8')
+    # sobel_y = Sobel(img, -1, 0, 1)
+    # kernel_y = np.array([[[1]] * 15], dtype='uint8')
 
     # closing y-axis
-    sobel_y = cv2.dilate(sobel_y, kernel_y)
+    # sobel_y = dilate(sobel_y, kernel_y)
 
     # generate mask for x
-    contours, hierarchy = cv2.findContours(
-        sobel_y, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    sorted_contours = sorted(contours, cmp=cmp_width)
+    # contours, hierarchy = findContours(
+    #     sobel_y, RETR_TREE, CHAIN_APPROX_SIMPLE)
+    # sorted_contours = sorted(contours, cmp=cmp_width)
 
     # fill biggest 10 on mask
-    mask_y = np.zeros(img.shape, np.uint8)
-    for c in sorted_contours[:10]:
-        cv2.drawContours(mask_y, [c], 0, 255, -1)
+    # mask_y = np.zeros(img.shape, np.uint8)
+    # for c in sorted_contours[:10]:
+    #     drawContours(mask_y, [c], 0, 255, -1)
 
-    cv2.imshow('matrix', cv2.bitwise_and(mask_x, mask_y))
+    # imshow('matrix', bitwise_and(mask_x, mask_y))
+
+    imshow('Input', frame)
+
+
+def solve_sudoku_in_picture(filename):
+    pic = imread(filename, CV_LOAD_IMAGE_GRAYSCALE)
+    process(pic)
+    waitKey(0)
+
+
+def solve_sudoku_in_video():
+    cap = VideoCapture(0)
+    while(True):
+        _, frame = cap.read()
+        process(frame)
+        if waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+
 
 if __name__ == '__main__':
-    solve_sudoku_in_picture('pics/sudoku.jpg')
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # solve_sudoku_in_picture('pics/sudoku.jpg')
+    solve_sudoku_in_video()
+    destroyAllWindows()
